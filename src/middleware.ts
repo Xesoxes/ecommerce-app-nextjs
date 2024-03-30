eebe68760c794a1a82780fe588cfa71c1ec39123
Korexy <rayssankn@gmail.com>
2024-03-30 22:54:07 +0700

    update : admin authentication

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { isValidPassword } from "./lib/isValidPassword";
+
+export async function middleware(req: NextRequest) {
+  if ((await isAuthenticated(req)) === false) {
+    return new NextResponse("Unauthorized", {
+      status: 401,
+      headers: { "WWW-Authenticate": "Basic" },
+    });
+  }
+}
+
+async function isAuthenticated(req: NextRequest) {
+  const authheader =
+    req.headers.get("authorization") || req.headers.get("Authorization");
+
+  if (authheader == null) return false;
+
+  const [username, password] = Buffer.from(authheader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+
+  return (
+    username === process.env.ADMIN_USERNAME &&
+    (await isValidPassword(
+      password,
+      process.env.HASHED_ADMIN_PASSWORD as string
+    ))
+  );
+}
+
+export const config = {
+  mathcer: "/admin/:path*",
+};
